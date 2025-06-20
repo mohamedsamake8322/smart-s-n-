3da939a1ce6787f0e11fc45f497b4316fbb286eb
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-06-20 23:26:25 +0300

    yt

--- smartagro-efficientnet-resnet.py
+++ smartagro-efficientnet-resnet.py
@@ -1,5 +1,13 @@
-from huggingface_hub import upload_file
+from huggingface_hub import create_repo, upload_file
 
+# Étape 1 : Créer le dépôt si besoin
+create_repo(
+    repo_id="mohamedsamake8322/smartagro-efficientnet-resnet",
+    repo_type="model",
+    exist_ok=True  # Ne plante pas si le repo existe déjà
+)
+
+# Étape 2 : Uploader ton fichier .keras
 upload_file(
     path_or_fileobj="model/efficientnet_resnet.keras",
     path_in_repo="efficientnet_resnet.keras",
