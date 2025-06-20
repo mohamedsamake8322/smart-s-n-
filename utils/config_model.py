c93cfb3595bfcc4a72bbd809b48373ea81224e80
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-06-20 12:26:31 +0300

    yt

--- utils/config_model.py
+++ utils/config_model.py
@@ -1,13 +1,14 @@
 import os
-import requests
+import gdown
 
-MODEL_URL = "https://drive.google.com/uc?export=download&id=1mBKbOYqB6db3KDneEtSpcH9ywC55qfW_"
 MODEL_PATH = os.path.join("model", "efficientnet_resnet.keras")
+DRIVE_ID = "1mBKbOYqB6db3KDneEtSpcH9ywC55qfW_"
 
 def download_model_if_missing():
     if not os.path.exists(MODEL_PATH):
-        print("⬇️ Téléchargement du modèle depuis Google Drive...")
+        print("⬇️ Téléchargement du modèle depuis Google Drive avec gdown...")
         os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
-        with open(MODEL_PATH, "wb") as f:
-            f.write(requests.get(MODEL_URL).content)
-        print("✅ Modèle téléchargé avec succès.")
+        gdown.download(id=DRIVE_ID, output=MODEL_PATH, quiet=False)
+        print("✅ Modèle téléchargé.")
+
+__all__ = ["MODEL_PATH", "download_model_if_missing"]
