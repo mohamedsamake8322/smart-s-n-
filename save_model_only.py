c15026ef7720b745c7470a73b1e16bc42f680b65
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-06-20 19:15:03 +0300

    yt

--- save_model_only.py
+++
@@ -1,5 +0,0 @@
-from tensorflow import keras
-
-model = keras.models.load_model("model/efficientnet_resnet.keras", compile=False)
-model.save("model/efficientnet_resnet.keras", save_format="keras")
-print("✅ Modèle resauvegardé avec succès")
