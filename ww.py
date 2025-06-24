bb8bda240131573cd12369757abf4ccd48aa3b65
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-06-24 10:09:38 +0300

    yt

--- ww.py
+++ ww.py
@@ -1,14 +1,11 @@
 import os
 
-dossier_principal = r"C:\plateforme-agricole-complete-v2\illustrations"
+# Remplace ce chemin par le chemin de ton dossier principal
+dossier_principal = r"C:\plateforme-agricole-complete-v2\plantdataset\train"
 
 for nom in os.listdir(dossier_principal):
     ancien_chemin = os.path.join(dossier_principal, nom)
     if os.path.isdir(ancien_chemin):
-        nouveau_nom = nom.lower().title()
+        nouveau_nom = nom.lower().capitalize()
         nouveau_chemin = os.path.join(dossier_principal, nouveau_nom)
-        if ancien_chemin != nouveau_chemin:
-            print(f"Renommage: '{nom}' → '{nouveau_nom}'")
-            os.rename(ancien_chemin, nouveau_chemin)
-        else:
-            print(f"Aucun changement pour: '{nom}'")
+        os.rename(ancien_chemin, nouveau_chemin)
