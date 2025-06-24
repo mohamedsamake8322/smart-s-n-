3f3ab4629a21d6e8e0e6ba328ab5d922d19661dd
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-06-24 10:10:46 +0300

    tr

--- ww.py
+++ ww.py
@@ -1,7 +1,7 @@
 import os
 
 # Remplace ce chemin par le chemin de ton dossier principal
-dossier_principal = r"C:\plateforme-agricole-complete-v2\plantdataset\train"
+dossier_principal = r"C:\plateforme-agricole-complete-v2\plantdataset\val"
 
 for nom in os.listdir(dossier_principal):
     ancien_chemin = os.path.join(dossier_principal, nom)
