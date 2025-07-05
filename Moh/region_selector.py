08fac861b0ffb77ebe9cc71fb6e25e377ba0adf4
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-05 11:23:21 +0300

    yt

--- Moh/region_selector.py
+++
@@ -1,7 +0,0 @@
-from region_profile import REGION_PROFILES
-
-def get_region_by_country(country: str):
-    for region in REGION_PROFILES:
-        if country in region.countries:
-            return region
-    return None  # Par défaut, région non trouvée
