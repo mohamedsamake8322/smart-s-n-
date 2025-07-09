cce44c53587967fafd0bcbd399c37b7d706fdb2c
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-10 00:50:00 +0300

    hv

--- config/lang.py
+++ config/lang.py
@@ -5,16 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Initialise le client Google Translate avec ta clé API
 client = translate.Client.from_service_account_json(os.getenv("GOOGLE_KEY_PATH"))
 
-# Fonction universelle de traduction
 @lru_cache(maxsize=5000)
 def t(texte_fr: str, langue_cible: str = "fr") -> str:
     if langue_cible == "fr":
         return texte_fr
     try:
-        resultat = client.translate(texte_fr, target_language=langue_cible)
-        return resultat["translatedText"]
+        result = client.translate(texte_fr, target_language=langue_cible)
+        return result["translatedText"]
     except Exception:
         return texte_fr
