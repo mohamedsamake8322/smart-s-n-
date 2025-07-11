6931776905c7b06e4e5a8a76ccfd10492e1e3148
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-11 23:51:50 +0300

    uj

--- pages/12_Voice_Assistant.py
+++ pages/12_Voice_Assistant.py
@@ -8,8 +8,6 @@ from datetime import datetime
 from utils.voice_assistant import voice_assistant, expert_chatbot
 from utils.translations import translator
 import json
-
-
 st.set_page_config(page_title="Voice Assistant", page_icon="🗣️", layout="wide")
 
 # Language selection
