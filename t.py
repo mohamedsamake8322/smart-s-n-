2626e35069777787c10bedc00b304fe78554db3c
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-15 14:40:28 +0300

    uy

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Fichier à corriger
-file_path = r"C:\plateforme-agricole-complete-v2\weather_cleaned\weather_Djibouti.csv"
+file_path = r"C:\plateforme-agricole-complete-v2\weather_cleaned\weather_Equatorial.csv"
 
 try:
     # 🧪 Essai avec différents séparateurs
