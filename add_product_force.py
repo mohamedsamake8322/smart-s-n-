6e3a55af0414e5cc93c90a6f69a28cc03e611b7f
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-19 11:57:21 +0300

    m

--- add_product_force.py
+++ add_product_force.py
@@ -1,14 +1,14 @@
-from datacube.index import Index
-from datacube.model import DatasetType
+import datacube
 import yaml
 
 # Charger le fichier YAML
 with open("s2_l2a_definition.yaml", "r", encoding="utf-8") as f:
     definition = yaml.safe_load(f)
 
-# Créer un index
-index = Index()
+# Créer un Datacube et récupérer l'index
+dc = datacube.Datacube(app='force-add-product')
+index = dc.index
 
-# Injecter via l'API
+# Injecter le produit
 index.products.add_document(definition)
 print("✅ Produit ajouté via API Python")
