0af51485e8bf160ffd245515d3fd4392bdfd8a43
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-20 00:04:40 +0300

    m

--- smart_agro_tools/db_interface/connector.py
+++ smart_agro_tools/db_interface/connector.py
@@ -1,4 +1,18 @@
-from .connector import connect_db  # Import relatif
+import psycopg2
+def connect_db(host, dbname, user, password, port):
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            dbname=dbname,
+            user=user,
+            password=password,
+            port=port
+        )
+        print("Connexion réussie à la base PostgreSQL ✅")
+        return conn
+    except Exception as e:
+        print("Erreur de connexion ❌ :", e)
+        return None
 
 conn = connect_db(
     host="localhost",
