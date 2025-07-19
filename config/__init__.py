4c9c92c6813de635af8813bd64b208251f28d0dc
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-07-19 23:10:48 +0300

    m

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main import app
-from .translation import translate_text, SUPPORTED_LANGUAGES
-from .language_manager import get_user_language, set_user_language
-
-__all__ = ['app', 'translate_text', 'SUPPORTED_LANGUAGES', 'get_user_language', 'set_user_language']
