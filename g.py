8caa4c519e4fc2c7de6057f5beefa437f5136e5f
Mohamed SAMAKE <ton-email-associ--mohamedsamake8322>
2025-08-12 19:57:15 +0300

    m

--- g.py
+++ g.py
@@ -85,6 +85,9 @@ payload = {
             "from": "2023-01-01T00:00:00Z",
             "to": "2023-12-31T23:59:59Z"
         },
+        "aggregationInterval": {
+            "of": "P1Y"
+        },
         "resx": 10,
         "resy": 10
     },
